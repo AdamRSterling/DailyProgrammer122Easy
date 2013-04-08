81cdc3a80034f59b49d1855c8e3b7808e73cb1ee
AdamRSterling <61sterling@cardinalmail.cua.edu>
2013-04-07 23:25:12 -0400

    added actual source code

---
+++ dp122.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int findDigitalRoot(number)
+{
+	while(number > 10)
+		number = number % 10 + findDigitalRoot(number / 10);
+	return number;
+}
+
+int main(int argc, char *argv[])
+{
+	int digitalRoot = findDigitalRoot(atoi(argv[1]));
+	printf("Digital root: %d", digitalRoot);
+	return 0;
+}
